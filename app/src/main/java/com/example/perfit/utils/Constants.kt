1b97ada7d09fe01def6ca1c3ea39cf8e4ad37d9a
Jing Liu <j762liu@uwaterloo.ca>
2023-02-16 15:40:42 -0500

    Added demo videos

--- app/src/main/java/com/example/perfit/utils/Constants.kt
+++ app/src/main/java/com/example/perfit/utils/Constants.kt
@@ -27,14 +27,20 @@ class Constants {
         private const val ACTION6 = "Jumping Jack"
         private const val DESC1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
                 "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        private val URI1 = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.basketball
+//        private val URI1 = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.basketball
+        private val URI_PUSH_UP = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.push_up_demo
+        private val URI_SIT_UP = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.sit_up_demo
+        private val URI_SQUAT = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.squat_demo
+        private val URI_PULL_UP = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.pull_up_demo
+        private val URI_PLANK = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.plank_demo
+        private val URI_JUMPING_JACK = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.jumping_jack_demo
 
-        val FITNESS_ACTIONS = listOf(FitnessActions(ACTION1, EASY, DESC1, URI1),
-            FitnessActions(ACTION2, MEDIUM, DESC1, URI1),
-            FitnessActions(ACTION3, HARD, DESC1, URI1),
-            FitnessActions(ACTION4, EASY, DESC1, URI1),
-            FitnessActions(ACTION5, MEDIUM, DESC1, URI1),
-            FitnessActions(ACTION6, HARD, DESC1, URI1))
+        val FITNESS_ACTIONS = listOf(FitnessActions(ACTION1, EASY, DESC1, URI_PUSH_UP),
+            FitnessActions(ACTION2, MEDIUM, DESC1, URI_SIT_UP),
+            FitnessActions(ACTION3, HARD, DESC1, URI_SQUAT),
+            FitnessActions(ACTION4, EASY, DESC1, URI_PULL_UP),
+            FitnessActions(ACTION5, MEDIUM, DESC1, URI_PLANK),
+            FitnessActions(ACTION6, HARD, DESC1, URI_JUMPING_JACK))
 
         // ROOM Database
         const val DATABASE_NAME = "fitness_results_database"
