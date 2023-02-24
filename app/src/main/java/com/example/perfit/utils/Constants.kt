6e6afa21dcc71aec1e800e83c05a79b0d2c1b0e6
Jing Liu <j762liu@uwaterloo.ca>
2023-02-23 23:09:37 -0500

    Added action descriptions

--- app/src/main/java/com/example/perfit/utils/Constants.kt
+++ app/src/main/java/com/example/perfit/utils/Constants.kt
@@ -25,9 +25,14 @@ class Constants {
         private const val ACTION4 = "Pull-Up"
         private const val ACTION5 = "Plank"
         private const val ACTION6 = "Jumping Jack"
-        private const val DESC1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-//        private val URI1 = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.basketball
+
+        private const val DESC_PUSH_UP = "Push-up is a common strength training exercise that mainly targets the muscles of the chest, shoulders, and triceps."
+        private const val DESC_SIT_UP = "Sit up is an exercise that involves lying on one's back with knees bent and feet flat on the ground, then lifting the upper body off the ground by contracting the abdominal muscles."
+        private const val DESC_SQUAT = "Squat is a lower body exercise that primarily targets the muscles of the hips, thighs, and glutes."
+        private const val DESC_PULL_UP = "A pull-up is a strength-training exercise that involves lifting oneself up using an overhead bar while hanging with arms fully extended."
+        private const val DESC_PLANK = "Plank is a bodyweight exercise that strengthens the core muscles, including the abs, back, and hips."
+        private const val DESC_JUMPING_JACK = "Jumping jack is a full-body exercise that involves jumping and spreading both arms and legs to the side at the same time, then returning to the starting position."
+
         private val URI_PUSH_UP = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.push_up_demo
         private val URI_SIT_UP = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.sit_up_demo
         private val URI_SQUAT = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.squat_demo
@@ -35,12 +40,14 @@ class Constants {
         private val URI_PLANK = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.plank_demo
         private val URI_JUMPING_JACK = "android.resource://" + BuildConfig.APPLICATION_ID + "/" + R.raw.jumping_jack_demo
 
-        val FITNESS_ACTIONS = listOf(FitnessActions(ACTION1, EASY, DESC1, URI_PUSH_UP),
-            FitnessActions(ACTION2, MEDIUM, DESC1, URI_SIT_UP),
-            FitnessActions(ACTION3, HARD, DESC1, URI_SQUAT),
-            FitnessActions(ACTION4, EASY, DESC1, URI_PULL_UP),
-            FitnessActions(ACTION5, MEDIUM, DESC1, URI_PLANK),
-            FitnessActions(ACTION6, HARD, DESC1, URI_JUMPING_JACK))
+        val FITNESS_ACTIONS = listOf(
+            FitnessActions(ACTION1, EASY, DESC_PUSH_UP, URI_PUSH_UP),
+            FitnessActions(ACTION2, MEDIUM, DESC_SIT_UP, URI_SIT_UP),
+            FitnessActions(ACTION3, HARD, DESC_SQUAT, URI_SQUAT),
+            FitnessActions(ACTION4, EASY, DESC_PULL_UP, URI_PULL_UP),
+            FitnessActions(ACTION5, MEDIUM, DESC_PLANK, URI_PLANK),
+            FitnessActions(ACTION6, HARD, DESC_JUMPING_JACK, URI_JUMPING_JACK)
+        )
 
         // ROOM Database
         const val DATABASE_NAME = "fitness_results_database"
